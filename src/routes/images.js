e33a25daa5f7d03b45b788c146a55910ca924ec7
franco4457 <franco_forex@hotmail.com>
2023-03-29 15:46:08 -0300

    delete clg

--- src/routes/images.js
+++ src/routes/images.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Router } = require("express");
 const express = require("express");
 const cloudinary = require("cloudinary").v2;
-const multerImages  = require("../helpers/multerImages.js");
+const multerImages = require("../helpers/multerImages.js");
 
 const {
-  SV_HOST,
+//   SV_HOST,
   CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
@@ -23,18 +23,17 @@ const imgroute = Router();
 imgroute.post("/", multerImages.single("image"), async (req, res) => {
   // const pathImage= `${SV_HOST}/images/${req.file.filename}`
   try {
-    const imageUrl = await cloudinary.uploader.upload(req.file.path, {
+    const { url } = await cloudinary.uploader.upload(req.file.path, {
       folder: "Pi-Dogs",
     });
-    console.log(imageUrl);
     res.status(200).json({
       msg: "image successfully changed",
-      url: imageUrl.url,
+      url,
     });
   } catch (error) {
     res.status(400).json({ msg: error.message });
   }
 });
-imgroute.use("/", express.static(`${__dirname}/../images`));
+// imgroute.use("/", express.static(`${__dirname}/../images`));
 
 module.exports = imgroute;
